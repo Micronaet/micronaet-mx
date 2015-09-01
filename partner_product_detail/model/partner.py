3e08f90261115db87ca3ecb2178542de1ad51b00
nicothebrush <nicola.riolini@gmail.com>
2015-09-01 11:35:19 +0200

    [ADD] partner_product_detail

---
+++ partner_product_detail/model/partner.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    Original module for stock.move from:
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
+
+_logger = logging.getLogger(__name__)
+
+
+class PartnerProductParticularity(orm.Model):
+    ''' Class for manage partucularity for partner - product
+    '''
+    
+    _name = 'partner.product.particularity'
+    _description = 'Partner product'
+    _rec_name = 'product_id'
+    _order = 'product_id'
+
+    _columns = {
+        'pricelist_report': fields.boolean('Pricelist report', 
+            help='For a pricelist report customized'),
+
+        'product_id': fields.many2one('product.product', 'Product'),
+        'alias_id': fields.many2one('product.product', 'Alias'),
+
+        'date': fields.datetime('Date'),
+        'pack_qty': fields.float('Pack q.ty', digits=(16, 2)),            
+        'packaging_id': fields.many2one('product.packaging', 'Packaging'),
+
+        'partner_id': fields.many2one('res.partner', 'Partner'),
+        'note': fields.text('Note'),
+        }
+
+    _defaults = {
+        'date': lambda *a: datetime.now().strftime(
+            DEFAULT_SERVER_DATE_FORMAT),
+        }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
