f2229fd3d7eb34c8d4c1261771a1ec0e4ea00381
nicothebrush <nicola.riolini@gmail.com>
2022-01-31 09:07:05 +0100

    pep 8 normalitazion

--- partner_product_detail/model/partner.py
+++ partner_product_detail/model/partner.py
@@ -23,6 +23,7 @@
 ###############################################################################
 
 import os
+import pdb
 import sys
 import logging
 import openerp
@@ -35,9 +36,9 @@ from openerp import SUPERUSER_ID
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
@@ -45,8 +46,8 @@ _logger = logging.getLogger(__name__)
 
 
 class ResPartnerPpricelistProduct(orm.Model):
-    ''' Class for manage particularity for partner - product
-    '''    
+    """ Class for manage particularity for partner - product
+    """
     _name = 'res.partner.pricelist.product'
     _description = 'Partner product'
     _rec_name = 'product_id'
@@ -58,20 +59,20 @@ class ResPartnerPpricelistProduct(orm.Model):
 
         'date': fields.date('Date'),
         'deadline': fields.date('Deadline'),
-        #'sell': fields.boolean('Sell rule'),
-        #'cost': fields.float('Cost', digits=(16, 2)), # for purchase?
+        # 'sell': fields.boolean('Sell rule'),
+        # 'cost': fields.float('Cost', digits=(16, 2)), # for purchase?
         'price': fields.float('Price', digits=(16, 2)),
         # TODO Currency
         'partner_id': fields.many2one('res.partner', 'Partner'),
         'note': fields.text('Note'),
-            
-        'packaging_id': fields.many2one('product.packaging', 'Packaging', 
+
+        'packaging_id': fields.many2one('product.packaging', 'Packaging',
             ondelete='set null'),
         # ---------------------------------------------------------------------
         # TODO used for calculate packaging? XXX not used for now
-        'product_ul': fields.many2one('product.ul', 'Pack', 
+        'product_ul': fields.many2one('product.ul', 'Pack',
             ondelete='set null'),
-        'load_qty': fields.float('Load q.ty', digits=(16, 2)),            
+        'load_qty': fields.float('Load q.ty', digits=(16, 2)),
         # ---------------------------------------------------------------------
         }
 
@@ -80,70 +81,72 @@ class ResPartnerPpricelistProduct(orm.Model):
             DEFAULT_SERVER_DATE_FORMAT),
         }
 
+
 class ResPartner(orm.Model):
-    ''' Class partner
-    '''    
+    """ Class partner
+    """
     _inherit = 'res.partner'
-    
+
     _columns = {
         'pricelist_product_ids': fields.one2many(
-            'res.partner.pricelist.product', 'partner_id', 
+            'res.partner.pricelist.product', 'partner_id',
             'Pricelist products'),
         }
 
+
 class SaleOrderLine(orm.Model):
-    ''' Add event for onchange in sale.order.line
-    '''    
+    """ Add event for onchange in sale.order.line
+    """
     _inherit = 'sale.order.line'
-    
+
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
-            lang=False, update_tax=True, date_order=False, packaging=False, 
+            lang=False, update_tax=True, date_order=False, packaging=False,
             fiscal_position=False, flag=False, context=None):
-        ''' Override function for set up extra fields as partner customization
-        '''    
+        """ Override function for set up extra fields as partner customization
+        """
         context = context or {}
         res = super(SaleOrderLine, self).product_id_change(
             cr, uid, ids, pricelist=pricelist, product=product, qty=qty,
-            uom=uom, qty_uos=qty_uos, uos=uos, name=name, 
-            partner_id=partner_id, lang=lang, update_tax=update_tax, 
-            date_order=date_order, packaging=packaging, 
+            uom=uom, qty_uos=qty_uos, uos=uos, name=name,
+            partner_id=partner_id, lang=lang, update_tax=update_tax,
+            date_order=date_order, packaging=packaging,
             fiscal_position=fiscal_position, flag=flag, context=context)
- 
+
         if 'value' not in res:
             res['value'] = {}
-        
+        pdb.set_trace()
         # Reset if partner or product not present:
         if not partner_id or not product:
             res['value'].update({
-                'alias_id': False,                
+                'alias_id': False,
                 'price_unit': False,
                 'product_packaging': False,
                 'load_qty': False, # XXX remove?
                 # TODO
                 })
             return res
-        
+
         # Used pool:
         partner_pool = self.pool.get('res.partner')
-        
+
         # Udpate field instead:
         partner_proxy = partner_pool.browse(
             cr, uid, partner_id, context=context)
-            
+
         for item in partner_proxy.pricelist_product_ids:
             if item.product_id.id == product:
                 res['value'].update({
                     'alias_id': item.alias_id.id,
                     'price_unit': item.price,
                     # TODO use first if not present in customization?
-                    'product_packaging': item.packaging_id.id, 
+                    'product_packaging': item.packaging_id.id,
                     'load_qty': item.load_qty, #XXX remove?
                     })
         return res
-    
+
     _columns = {
         # TODO remove?
-        'load_qty': fields.float('Load q.ty', digits=(16, 2)),            
-        }        
+        'load_qty': fields.float('Load q.ty', digits=(16, 2)),
+        }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
